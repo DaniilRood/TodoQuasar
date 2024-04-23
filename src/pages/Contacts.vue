98c3da376866a97ce6a1cd0db83a091ac3e277d1
Daniil Lysykh <lysykh_daniil@mail.ru>
2024-04-23 12:55:26 +0300

    Верстка + Стили + Переключение по страницам

---
+++ src/pages/Contacts.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page>
+    <h5 class="title">Contacts</h5>
+    <p class="text">
+      Для связи с нами напишите на почту: <a class="link" href="mailto:lysykh_daniil@mail.ru">lysykh_daniil@mail.ru</a>
+    </p>
+
+  </q-page>
+</template>
+
+<script>
+
+</script>
+
+<style>
+  .text {
+    font-size: 14px;
+    text-align: justify;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    font-family: 'Gotham Pro';
+    padding: 20px;
+  }
+  .title {
+    text-align: center;
+  }
+  .link {
+    text-decoration: none;
+    color: gray;
+  }
+</style>
---
+++ src/pages/Help.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page>
+    <h5 class="title">Help</h5>
+    <p class="text">
+      Если у вас возникли вопросы напишите на почту: <a class="link" href="mailto:lysykh_daniil@mail.ru">lysykh_daniil@mail.ru</a>
+    </p>
+  </q-page>
+</template>
+
+<script>
+
+</script>
+
+<style>
+  .text {
+    font-size: 14px;
+    text-align: justify;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    font-family: 'Gotham Pro';
+    padding: 20px;
+  }
+  .title {
+    text-align: center;
+  }
+  .link {
+    text-decoration: none;
+    color: gray;
+  }
+</style>
